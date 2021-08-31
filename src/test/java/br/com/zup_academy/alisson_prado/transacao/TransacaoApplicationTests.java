781ddb9f35289fdef620844c0dcfbb2a0df8f2a8
Alisson Prado <alisson.prado@zup.com.br>
2021-08-31 08:16:20 -0300

    setup-projeto

---
+++ src/test/java/br/com/zup_academy/alisson_prado/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup_academy.alisson_prado.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
